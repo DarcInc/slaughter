7fd1fc7faf4278faf2c64912d8f359a3caad9503
Paul Hoehne <paul.hoehne@gmail.com>
2022-08-28 22:55:28 -0400

    Placeholder for Encryptor.

---
+++ stashing/AesEncryptor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by phoehne on 8/28/22.
+//
+
+#include "AesEncryptor.hpp"
---
+++ stashing/AesEncryptor.hpp
@@ -0,0 +1,14 @@
+//
+// Created by phoehne on 8/28/22.
+//
+
+#ifndef SLAUGHTER_AESENCRYPTOR_HPP
+#define SLAUGHTER_AESENCRYPTOR_HPP
+
+
+class AesEncryptor {
+
+};
+
+
+#endif //SLAUGHTER_AESENCRYPTOR_HPP
